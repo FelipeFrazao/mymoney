7be6fc7cf60af02af0a40ca4fbad1052209de394
Felipe Frazão <felipe.sfrazao@outlook.com>
2017-12-13 15:52:06 -0200

    Initial commit

---
+++ app/src/main/java/frazao/felipe/mymoney/ui/activity/ListaTransacoesActivity.kt
@@ -0,0 +1,18 @@
+package frazao.felipe.mymoney.ui.activity
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.support.v7.app.AppCompatActivity
+import frazao.felipe.mymoney.R
+
+/**
+ * Created by felipefrazao on 13/12/2017.
+ */
+class ListaTransacoesActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lista_transacoes)
+    }
+
+}
