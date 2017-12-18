fed38b6d242440817d601b3d277696ec940abf62
Felipe Frazão <felipe.sfrazao@outlook.com>
2017-12-18 12:02:11 -0200

    Exibicao de cor se o total é positivo ou negativo

--- app/src/main/java/frazao/felipe/mymoney/ui/ResumoView.kt
+++ app/src/main/java/frazao/felipe/mymoney/ui/ResumoView.kt
@@ -20,28 +20,36 @@ class ResumoView (private val view: View,
 
     var totalReceita = BigDecimal.ZERO
     var totalDespesa = BigDecimal.ZERO
-
     private val resumo: Resumo = Resumo(transacoes)
 
     fun adicionaReceitaNoResumo() {
-
         totalReceita = resumo.receita()
-
         view.resumo_card_receita.text = totalReceita.formataParaBR()
-
         view.resumo_card_receita.setTextColor(ContextCompat.getColor(context, R.color.receita))
 
     }
 
     fun adicionarDespesaNoResumo() {
-
         totalDespesa = resumo.despesa()
         view.resumo_card_despesa.text = totalDespesa.formataParaBR()
         view.resumo_card_despesa.setTextColor(ContextCompat.getColor(context, R.color.despesa))
 
     }
     fun totalFinancas() {
+        var cor : Int
         val total = resumo.total()
+
+        when (total < BigDecimal.ZERO) {
+            true -> cor = ContextCompat.getColor(context, R.color.despesa)
+            false -> cor = ContextCompat.getColor(context, R.color.receita)
+        }
+//        if (total < BigDecimal.ZERO) {
+//            cor = ContextCompat.getColor(context, R.color.despesa)
+//        } else {
+//            cor = ContextCompat.getColor(context, R.color.receita)
+//        }
+
+        view.resumo_card_total.setTextColor(cor)
         view.resumo_card_total.text = total.formataParaBR()
     }
 }
