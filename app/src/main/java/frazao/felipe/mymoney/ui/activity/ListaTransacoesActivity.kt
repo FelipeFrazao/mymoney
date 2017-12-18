16879c54540703274875724bd99dd9775d12e91d
Felipe Frazão <felipe.sfrazao@outlook.com>
2017-12-18 15:48:45 -0200

    Buttons adicionar receita e adicionar depesa estao chamando um dialog

--- app/src/main/java/frazao/felipe/mymoney/ui/activity/ListaTransacoesActivity.kt
+++ app/src/main/java/frazao/felipe/mymoney/ui/activity/ListaTransacoesActivity.kt
@@ -1,8 +1,12 @@
 package frazao.felipe.mymoney.ui.activity
 
 import android.os.Bundle
+import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
 import frazao.felipe.mymoney.R
 import frazao.felipe.mymoney.model.Tipo
 import frazao.felipe.mymoney.model.Transacao
@@ -53,5 +57,26 @@ class ListaTransacoesActivity : AppCompatActivity() {
 
         // configurando o adapter
         configuraLista()
+
+        lista_transacoes_adiciona_receita.setOnClickListener {
+            val view: View = window.decorView
+
+            val viewCriada = LayoutInflater.from(this).inflate(R.layout.form_transacao, view as ViewGroup, false)
+            AlertDialog.Builder(this)
+                    .setTitle(R.string.adiciona_receita)
+                    .setView(viewCriada)
+                    .show()
+        }
+        lista_transacoes_adiciona_despesa.setOnClickListener {
+            val view: View = window.decorView
+
+            val viewCriada = LayoutInflater.from(this).inflate(R.layout.form_transacao, view as ViewGroup, false)
+            AlertDialog.Builder(this)
+                    .setTitle(R.string.adiciona_despesa)
+                    .setView(viewCriada)
+                    .show()
+        }
     }
+
+
 }
