342fd552aee08817ec63f4e52b4c93c399da0f24
Felipe Frazão <felipe.sfrazao@outlook.com>
2017-12-19 09:55:11 -0200

    Layout do dialog de adicionar transacao implementado

--- app/src/main/java/frazao/felipe/mymoney/ui/activity/ListaTransacoesActivity.kt
+++ app/src/main/java/frazao/felipe/mymoney/ui/activity/ListaTransacoesActivity.kt
@@ -4,12 +4,10 @@ import android.app.DatePickerDialog
 import android.os.Bundle
 import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
-import android.support.v7.widget.DialogTitle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.DatePicker
-import android.widget.Toast
+import android.widget.ArrayAdapter
 import frazao.felipe.mymoney.R
 import frazao.felipe.mymoney.extension.formataParaBR
 import frazao.felipe.mymoney.model.Tipo
@@ -65,35 +63,41 @@ class ListaTransacoesActivity : AppCompatActivity() {
         configuraLista()
 
         lista_transacoes_adiciona_receita.setOnClickListener {
-            abreDialog(R.string.adiciona_receita)
+            abreDialog(R.string.adiciona_receita, R.array.categorias_de_receita)
         }
         lista_transacoes_adiciona_despesa.setOnClickListener {
-            abreDialog(R.string.adiciona_despesa)
+            abreDialog(R.string.adiciona_despesa, R.array.categorias_de_despesa)
         }
     }
 
-    private fun abreDialog(title: Int) {
+    private fun abreDialog(title: Int, category: Int) {
         val dia = 18
         val mes = 11
         val ano = 2017
         val view: View = window.decorView
         val viewCriada = LayoutInflater.from(this).inflate(R.layout.form_transacao, view as ViewGroup, false)
         val hoje = Calendar.getInstance()
-        viewCriada.form_transacao_data.setText(hoje.formataParaBR())
-        viewCriada.form_transacao_data.setOnClickListener{
-            DatePickerDialog(this,
-                    DatePickerDialog.OnDateSetListener { view, ano, mes, dia ->
-                        val dataSelecionada = Calendar.getInstance()
-                        dataSelecionada.set(ano, mes, dia)
-                        viewCriada.form_transacao_data.setText(dataSelecionada.formataParaBR())
-                    }, ano, mes, dia).show()
+        with (viewCriada) {
+            form_transacao_data.setText(hoje.formataParaBR())
+            form_transacao_data.setOnClickListener{
+                DatePickerDialog(this@ListaTransacoesActivity,
+                        DatePickerDialog.OnDateSetListener { view, ano, mes, dia ->
+                            val dataSelecionada = Calendar.getInstance()
+                            dataSelecionada.set(ano, mes, dia)
+                            form_transacao_data.setText(dataSelecionada.formataParaBR())
+                        }, ano, mes, dia).show()
+            }
+            val adapter = ArrayAdapter.createFromResource(this@ListaTransacoesActivity, category, android.R.layout.simple_spinner_dropdown_item)
+
+            form_transacao_categoria.adapter = adapter
+
+
+            AlertDialog.Builder(this@ListaTransacoesActivity)
+                    .setTitle(title)
+                    .setView(viewCriada)
+                    .setNegativeButton("Cancelar", null)
+                    .setPositiveButton("Adicionar", null)
+                    .show()
         }
-
-        AlertDialog.Builder(this)
-                .setTitle(title)
-                .setView(viewCriada)
-                .show()
     }
-
-
 }
